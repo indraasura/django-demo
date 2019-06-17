3300d9a6622cbeccfd578b083aec94caafe4cd02
Swarup <swaruphegde@gmail.com>
2019-06-17 23:35:53 +0530

    added namespace removed hardcoded hrefs

--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Question
+from django.shortcuts import get_object_or_404
 
 def index(request):
     latest_question_list = Question.objects.order_by('-published_date')[:5]
@@ -7,7 +8,8 @@ def index(request):
     return render(request, 'polls/index.html', context)
 
 def detail(request, question_id):
-    return HttpResponse("You are looking at question %s" % question_id)
+   question = get_object_or_404(Question, pk=question_id)
+   return(request, 'polls/detail.html', {'question':question})
 
 def results(request, question_id):
     response = "You are looking at the results of question %s"

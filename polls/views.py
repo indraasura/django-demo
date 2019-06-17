0ce4c5530a837827f53d6607b22e7ecf80ddc39c
Swarup <swaruphegde@gmail.com>
2019-06-17 10:05:11 +0530

    wanna go to templates

--- polls/views.py
+++ polls/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Question
 
 def index(request):
-    return HttpResponse("Hello world")
+    latest_question_list = Question.objects.order_by('-published_date')[:5] 
+    output = ', '.join([q.question_text for q in latest_question_list])
+    return HttpResponse(output)
 
 def detail(request, question_id):
     return HttpResponse("You are looking at question %s" % question_id)
@@ -12,5 +15,4 @@ def results(request, question_id):
     return HttpResponse(response % question_id)
 
 def vote(request, question_id):
-    return HttpResponse("You are voting on question %s" % question_id)
-
+    return HttpResponse("You are voting at question %s" % question_id)
